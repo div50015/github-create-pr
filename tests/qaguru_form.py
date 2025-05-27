28c4a09c94c804ce3e2376b43bf132a0db499b35
Igor Degtyarenko <div50015@mail.ru>
2025-05-27 15:17:42 +0300

    edit  test_registration_form - fill gender

--- tests/qaguru_form.py
+++ tests/qaguru_form.py
@@ -11,6 +11,8 @@ def test_registration_form():
     browser.element('#firstName').type('Igor')
     browser.element('#lastName').type('Degtyarenko')
     browser.element('#userEmail').type('div@novoch.ru')
-    browser.all('[name=gender]').element('[value=Male]')
+    # browser.element('[name=gender][value=Female]+label').click()
+    # browser.all('[name=gender]').element_by(have.value('Female')).element('..').click()
+    browser.all('[for^=gender-radio]').element_by(have.text('Female')).click()
     time.sleep(3)
 
